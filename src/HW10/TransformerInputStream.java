b181b891882c175cdaffce21b15e666e489a1b2b
Juliia Semykina <semykinayuliia@gmail.com>
2022-07-23 17:15:03 +0300

    + private static class

--- src/HW10/TransformerInputStream.java
+++ src/HW10/TransformerInputStream.java
@@ -1,25 +1,40 @@
 package HW10;
 
 import java.io.*;
-public class TransformerInputStream extends FilterInputStream {
-    private char from, to;
-    protected TransformerInputStream(InputStream in, char from, char to) {
 
+public class TransformerInputStream extends FilterInputStream {
+    private static class Variables {
+
+        private static char from;
+        private static char to;
+
+        public static char getFrom() {
+            return from;
+        }
+        public static char getTo(){
+            return to;
+        }
+    }
+
+    protected TransformerInputStream(InputStream in, char from, char to) {
         super(in);
-        this.from = from;
-        this.to = to;
+        Variables.from = from;
+        Variables.to = to;
     }
     @Override
     public int read() throws IOException {
         int s = super.read();
-        if (s == from) {
-            s = to;
+        if(s == -1){
+            return s;
+        }
+        if (s == Variables.getFrom()) {
+            s = Variables.getTo();
         }
         return s;
     }
 
     public static void main(String[] args) {
-        try (TransformerInputStream tis = new TransformerInputStream(new TransformerInputStream(new TransformerInputStream(System.in, 'q', 'w'), 'z', 'x'),'v','p')) {
+        try (TransformerInputStream tis = new TransformerInputStream(new TransformerInputStream(System.in, 'q', 'w'), 'z', 'x')) {
             int read;
             while ((read = tis.read()) != -1) {
                 System.out.print((char) read);
@@ -28,6 +43,7 @@ public class TransformerInputStream extends FilterInputStream {
             System.err.println(e.getMessage());
         }
     }
+
 }
 
 
